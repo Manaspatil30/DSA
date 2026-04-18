d2c80f93474079c8a5fa834a85b5ba8f9fd00786
Manas Patil <89078720+Manaspatil30@users.noreply.github.com>
2026-04-18 14:40:25 +0100

    Add Armstrong Number

---
+++ src/math/Armstrong.java
@@ -0,0 +1,35 @@
+package math;
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int original = n;
+
+        //Step 1 : Count digits
+        int count = 0;
+        int temp = n;
+        while (temp > 0){
+            count++;
+            temp = temp/10;
+        }
+
+        //Step 2 & 3: Extract digits and compute sum
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += (int) Math.pow(digit, count);
+            n = n/10;
+        }
+
+        //Step 4 : Compare
+        if(sum == original){
+            System.out.println(original + " is a Armstrong Number");
+        } else {
+            System.out.println(original + " is NOT a Armstrong Number");
+        }
+        sc.close();
+    }
+}
