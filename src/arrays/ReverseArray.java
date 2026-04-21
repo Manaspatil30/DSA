2be57b2bcfd3f15684efca60a6f7d0b202b8979e
Manas Patil <89078720+Manaspatil30@users.noreply.github.com>
2026-04-21 18:07:34 +0100

    Add ReverseArray

---
+++ src/arrays/ReverseArray.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        //Read array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0; //start index
+        int right = arr.length - 1;  //end index
+
+        while (left < right){
+            //Swap
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            //Move pointers
+            left++;
+            right--;
+        }
+
+        //Print result
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
